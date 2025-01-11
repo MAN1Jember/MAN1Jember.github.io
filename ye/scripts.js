37127e5ad12bdaca31d0d5e37bf3258ab2234c10
MAN1Jember <vantzyisreal@gmail.com>
2025-01-11 11:12:35 +0700

    Add files via upload

---
+++ ye/scripts.js
@@ -0,0 +1,5 @@
+// Mengarahkan ke halaman lain setelah beberapa detik
+setTimeout(() => {
+    window.location.href = "isi.html"; // Ganti dengan URL halaman tujuan Anda
+  }, 4000); // Redirect setelah 5 detik
+  
